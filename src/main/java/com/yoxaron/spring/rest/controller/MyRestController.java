ed3acce884756423e69adce139f64afbc2456445
yoxaron <andanotheronemailbox@gmail.com>
2023-08-11 17:10:03 +0100

    first commit

---
+++ src/main/java/com/yoxaron/spring/rest/controller/MyRestController.java
@@ -0,0 +1,55 @@
+package com.yoxaron.spring.rest.controller;
+
+import com.yoxaron.spring.rest.entity.Employee;
+import com.yoxaron.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.yoxaron.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.yoxaron.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee witch ID = " +
+                    id + " in database");
+        }
+
+        return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(
+            NoSuchEmployeeException exception) {
+
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(
+            Exception exception) {
+
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/yoxaron/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.yoxaron.spring.rest.dao;
+
+import com.yoxaron.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
